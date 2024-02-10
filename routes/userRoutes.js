c6cf5c69207426f40997dd7a393b734f6d2c3a74
BIKASH MALU <bikashmalu1@gmail.com>
2024-02-10 16:34:06 +0530

    2nd commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express=require('express')
 const {  getbill } = require('../controller/itemController');
 const router=express.Router();
-router.head('/bill',getbill);
+router.head('/bill',getbill)
 module.exports=router
